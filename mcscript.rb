86413ea8a85849990e2838c15a4d110754edb61f
koleq(NICOLEv2) <o.kurz@atlas.cz>
2022-01-28 16:35:23 +0100

    Fix re-extracting, update .gitignore

--- mcscript.rb
+++ mcscript.rb
@@ -6,7 +6,11 @@ mc_current = file.read  # reads stored status of .minecraft folder
 mc_switch = ARGV[0]  # archive to switch to
 mc_launcher_location = ARGV[1]  # path to minecraft executable or link
 
-if File.directory?("#{appdata}\\.minecraft")  # if there is a minecraft directory in appdata
+if mc_current == mc_switch
+	ready = true
+end
+
+if File.directory?("#{appdata}\\.minecraft") and not ready  # if there is a minecraft directory in appdata
     if File.exist?(".minecraft(#{mc_current}).7z")  # if there is already an archive
         system("del \".minecraft(#{mc_current}).7z\"")  # delete it
     end
@@ -17,11 +21,11 @@ end
 
 archive = File.exist?(".minecraft(#{mc_switch}).7z")
 
-if archive  # check if there is archive
+if archive and not ready  # check if there is archive
     system("7z x .minecraft(#{mc_switch}).7z -o\"#{appdata}\"")  # extract the archive to appdata
 end
 
-if archive or (!archive && mc_switch == "Vanilla")  # if there is archive or choice was Vanilla
+if ready or archive or (!archive && mc_switch == "Vanilla")  # if there is archive or choice was Vanilla
     File.open("MC_status.txt", "w") { |f| f.write mc_switch }  # change status to current
     system("start #{mc_launcher_location}")  # launch game
 end
